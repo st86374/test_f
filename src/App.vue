25dd1a6459215866e25c5e8fd4a4f29571d24e17
tom <tom@gastom.com.tw>
2022-01-24 01:06:43 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<header></header>
+<main>
+  <section class="banner">
+    
+  </section>
+</main>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="text-center">
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import '@/assets/css/main.scss';
+createApp(App).mount('#app')
